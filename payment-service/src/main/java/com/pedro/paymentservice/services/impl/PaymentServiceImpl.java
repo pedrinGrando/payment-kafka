b2147e71c618e448700006a90ef31f8d488006a8
Pedro Aluisio Scuissiatto <pedrogrando6@gmail.com>
2024-09-19 14:25:06 -0300

    Add producer config

--- payment-service/src/main/java/com/pedro/paymentservice/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/pedro/paymentservice/services/impl/PaymentServiceImpl.java
@@ -2,15 +2,28 @@ package com.pedro.paymentservice.services.impl;
 
 import com.pedro.paymentservice.model.Payment;
 import com.pedro.paymentservice.services.PaymentServices;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentServices {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-          log.info("PAYMENT_SERVICE_IMPL ::: Payment received {}", payment);
+          log.info("Payment received {}", payment);
+          Thread.sleep(1000);
+
+          log.info("Sending payment {}", payment);
+          kafkaTemplate.send("payment_topic", payment);
     }
 }
